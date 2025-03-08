cd5bf4781f1fc3f0370b2f09fc2918082c2d6c79
codak2 <165698263+codak2@users.noreply.github.com>
2025-03-08 16:41:00 +0000

    Basic list complete

--- index.js
+++ index.js
@@ -12,7 +12,26 @@ SVNameArray.forEach(sv => {
 console.log(SVOut);
 
 
+// list header + footer
+let listName = testData.listName;
+
+let listHeader = `<define label="${listName}">`;
+let listFooter = `\n</define>`;
+
+console.log(listHeader);
+
 
 // make a row
+// <row label="Br1" cs:cat="type1">Brand 1</row>
 
+let rowsArray = testData.listRows;
+let rowLabel = testData.rowLabel;
+let listRowsOut = '';
+
+rowsArray.forEach( (rowName,index) => {
+    listRowsOut += `<row label="${rowLabel}${index+1}" value="${index+1}">${rowName}</row>\n`;
+});
+
+
+console.log(listRowsOut, listFooter);
 
