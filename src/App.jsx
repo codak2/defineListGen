2ccdb0b322dbc3b0ab664dcd7ad014f320ff09c6
codak2 <165698263+codak2@users.noreply.github.com>
2025-03-25 19:24:20 +0000

    add basic frontend

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
       <main id='mainContainer'>
 
         <div id="headerContainer" className='container1'>
-
           <label htmlFor=""> List Name <br />
             <input type="text" placeholder='Enter List Name' />
           </label>
@@ -27,67 +26,47 @@ function App() {
           </label>
 
           <button type="button">Add Stylevar</button>
-
-        </div>
-
-<div className="contentContainer">
-
-        <div id="listContainer" className='container2'>
-
-          <label htmlFor=""> List Rows <br />
-            <textarea name="" id="" placeholder='Enter List Rows' cols="40" rows="40"></textarea>
-          </label>
-
-          <button type="button">Clear List Rows</button>
-
-
         </div>
 
 
+        <div className="contentContainer">
 
-        
+          <div id="listContainer" className='container2'>
+            <label htmlFor=""> List Rows <br />
+              <textarea name="" id="" placeholder='Enter List Rows' cols="40" rows="40"></textarea>
+            </label>
+
+            <button type="button">Clear List Rows</button>
+          </div>
 
 
-        <div id="stylevarContainer" className='container2'>
+          <div id="stylevarContainer" className='container2'>
+            <label htmlFor=""> Stylevar Values <br />
+              <textarea name="" id="" placeholder='Enter Stylevar Values' cols="40" rows="40"></textarea>
+            </label>
 
-          <div className="container1">
             <label htmlFor=""> Stylevar Name <br />
               <input type="text" placeholder='Enter Stylevar Name' />
             </label>
 
             <button type="button">Remove Stylevar</button>
-
           </div>
 
-          <label htmlFor=""> Stylevar Values <br />
 
-            <textarea name="" id="" placeholder='Enter Stylevar Values' cols="40" rows="40"></textarea>
+          <div id="outputContainer" className='container2' style={{ "width": "20rem" }}>
+            <label htmlFor=""> Output <br />
+              <textarea name="" id="" placeholder='Output will be generated here....' cols="40" rows="40"></textarea>
+            </label>
 
-          </label>
-
-
-
-
-        </div>
-
-        <div id="outputContainer" className='container2' >
-          <label htmlFor=""> Output <br />
-            <textarea name="" id="" placeholder='Output will be generated here....' cols="40" rows="40"></textarea>
-          </label>
-
-
-          <button type="button">Generate List</button>
-          <button type="button">Save to file</button>
-          <button type="button">Copy Output</button>
-          <button type="button">Clear Output</button>
+            <div className="container1">
+              <button type="button">Generate List</button>
+              <button type="button">Save to file</button>
+              <button type="button">Copy Output</button>
+              <button type="button">Clear Output</button>
+            </div>
+          </div>
 
         </div>
-
-        </div>
-
-
-
-
 
       </main>
     </>
