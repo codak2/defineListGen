0ed4993207e694766e6cdc044b4265b93c6ed959
codak2 <165698263+codak2@users.noreply.github.com>
2025-03-27 12:21:06 +0000

    Headercontainer component complete

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,111 @@ import OutputContainer from "./components/OutputContainer"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  let [listData, setListData] = useState({
+    listName: "BrandList",
+    styleVars: {
+      name: ["category", "country", "timezone"],
+      values: [
+        [
+          "type1",
+          "type1",
+          "type1",
+          "type1",
+          "type2",
+          "type2",
+          "type2",
+          "type2",
+        ],
+        ["US", "US", "US", "US", "UK", "UK", "UK", "UK"],
+        ["EST", "EST", "EST", "EST", "GMT", "GMT", "GMT", "GMT"],
+      ],
+    },
+    rowLabel: "Br",
+    isRowValues: true,
+    isCollapseDupes: true,
+    listRows: [
+      "Brand 1",
+      "Brand 2",
+      "Brand 3",
+      "Brand 4",
+      "Brand 5",
+      "Brand 6",
+      "Brand 7",
+      "Brand 8",
+    ],
+  });
+
+  let [output, setOutput] = useState([]);
+
+
+const handleListDataChange = (key, value) => {
+  setListData(prevState => ({
+    ...prevState,
+    [key]: value,
+  }));
+};
+
+
+const handleListNameChange = (e) => {
+  handleListDataChange('listName', e.target.value);
+};
+
+const handleRowLabelChange = (e) => {
+  handleListDataChange('rowLabel', e.target.value);
+};
+
+const handleAddRowValues = (e) => {
+  handleListDataChange('isRowValues', !listData.isRowValues)
+}
+
+const handleCollapseDupes = (e) => {
+  handleListDataChange('isCollapseDupes', !listData.isCollapseDupes)
+}
+
+const handleAddStylevar = () =>{
+  // TODO: handleAddStylevar
+  console.log("TODO: handleAddStylevar")
+}
+
+const handleListRowsChange = (e) => {
+  handleListDataChange('listRows', e.target.value.split('\n'));
+};
+
+const handleStyleVarChange = (e) => {
+  handleListDataChange('styleVars', {
+    ...listData.styleVars,
+    name: e.target.value.split(','),
+  });
+};
+
+const handleStyleVarValueChange = (e) => {
+  handleListDataChange('styleVars', {
+    ...listData.styleVars,
+    values: e.target.value.split(','),
+  });
+};
+
+
+
+
+
 
   return (
     <>
       <main id='mainContainer'>
 
-        <HeaderContainer /> 
+        <HeaderContainer 
+        listName={listData.listName}
+        handleListNameChange={handleListNameChange}
+        rowLabel={listData.rowLabel}
+        handleRowLabelChange={handleRowLabelChange}
+        isAddRowValues={listData.isRowValues}
+        handleAddRowValues={handleAddRowValues}
+        isCollapseDupes={listData.isCollapseDupes}
+        handleCollapseDupes={handleCollapseDupes}
+        handleAddStylevar={handleAddStylevar}
+        /> 
 
         <div className="contentContainer">
             <ListContainer />
--- src/components/HeaderContainer.jsx
+++ src/components/HeaderContainer.jsx
@@ -1,26 +1,43 @@
 import React from 'react'
 
-const HeaderContainer = () => {
+const HeaderContainer = (props) => {
   return (
     <>
             <div id="headerContainer" className='container1'>
           <label htmlFor=""> List Name <br />
-            <input type="text" placeholder='Enter List Name' />
+            <input 
+            type="text" 
+            placeholder='Enter List Name'
+            value={props.listName}
+            onChange={props.handleListNameChange}
+            />
           </label>
 
-          <label htmlFor=""> Row Prefix <br />
-            <input type="text" placeholder='Enter Row Prefix (e.g. Br)' />
+          <label htmlFor=""> Row Label Prefix <br />
+            <input 
+            type="text" 
+            placeholder='Enter Row Prefix (e.g. Br)' 
+            value={props.rowLabel}
+            onChange={props.handleRowLabelChange}
+            />
           </label>
 
           <label htmlFor="" className='center'> Add Row Values <br />
-            <input type="checkbox" />
+            <input 
+            type="checkbox" 
+            checked={props.isAddRowValues} 
+            onChange={props.handleAddRowValues}/>
           </label>
 
           <label htmlFor="" className='center'> Collapse Duplicates <br />
-            <input type="checkbox" />
+            <input type="checkbox"
+            checked={props.isCollapseDupes}
+            onChange={props.handleCollapseDupes} />
           </label>
 
-          <button type="button">Add Stylevar</button>
+          <button type="button"
+          onClick={props.handleAddStylevar}
+          >Add Stylevar</button>
         </div>
     </>
   )
