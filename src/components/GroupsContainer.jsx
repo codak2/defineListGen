041c68e38c3f2ba34c5d95524c83d44a85a7e085
codak2 <165698263+codak2@users.noreply.github.com>
2025-07-01 17:14:31 +0000

    added group label and name input fields in groupscontainer

--- src/components/GroupsContainer.jsx
+++ src/components/GroupsContainer.jsx
@@ -10,22 +10,76 @@ const GroupsContainer = (props) =>{
               placeholder='Enter Groups Values' 
               cols="30" 
               rows="30"
-              value={props.grpValList}
+              value={props.groups.groupValues.join("\n")}
             //   onChange={(e) => props.handleStyleVarValueChange(e, props.index)}
               />
             </label>
 
-            {/* <label htmlFor=""> Stylevar Name <br />
-              <input type="text" 
-              placeholder='Enter Stylevar Name'
-              value={props.SVName}
-              onChange={(e) => props.handleStyleVarChange(e, props.index)}
-              />
-            </label>
+
+{
+  props.groups.groupNames.map((grpName, index)=>{
+    return(
+                  <div>
+                    <label htmlFor=""> Group Label <br />
+                        <input type="text" 
+                        key={`${grpName.groupLabel}-${index}`}
+                        index={index}
+                        placeholder='Enter Group Label'
+                        value={grpName.groupLabel}
+                        />
+                      </label>
+                      <br />
+
+                      <label htmlFor=""> Group Name <br />
+                        <input type="text" 
+                        key={`${grpName.groupName}-${index}`}
+                        index={index+1}
+                        placeholder='Enter Group Name'
+                        value={grpName.groupName}
+                        />
+                      </label>
+                  </div>
+    )
+  })
+}
+
+
+        
+
+
+            {/* 
+            
+           
 
             <button type="button"
             onClick={() => props.handleRemoveStylevar(props.index)}
-            >Remove Stylevar</button> */}
+            >Remove Stylevar</button>
+            
+
+ <label htmlFor=""> Group Label <br />
+                        <input type="text" 
+                        key={`${grpName.groupLabel}-${index}`}
+                        index={index}
+                        placeholder='Enter Group Label'
+                        value={grpName.groupLabel}
+                        // onChange={(e) => props.handleStyleVarChange(e, props.index)}
+                        />
+                      </label>
+
+                      <label htmlFor=""> Group Name <br />
+                        <input type="text" 
+                        key={`${grpName.groupName}-${index}`}
+                        index={index}
+                        placeholder='Enter Group Name'
+                        value={grpName.groupName}
+                        // onChange={(e) => props.handleStyleVarChange(e, props.index)}
+                        />
+                      </label>
+                    
+            
+            
+            
+            */}
             
           </div>
         
